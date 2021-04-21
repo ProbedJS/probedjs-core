820911d2c3c5f9122f32aa167df913faeea45c05
Francois Chabot <francois.chabot.dev@gmail.com>
2021-04-21 10:06:57 -0400

    Static jest config to help older Node versions.

--- test-dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(disttest))\\.ts?$',
-    moduleFileExtensions: ['js', 'ts'],
-};
